cb0e919c97f2e78fd81f8bdb814fd7e46640378f
Harjap <harjapbhatia3@gmail.com>
2025-01-15 15:45:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.8 MB (95.28%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen = {}
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hm = {}
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+            diff = target - num
+            if diff in hm:
+                return [hm[diff],i]
+            hm[num] = i
 
