349a06da434df8a2f983af546734ef9fc59facde
Harjap <harjapbhatia3@gmail.com>
2025-09-16 12:58:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (21.20%)

--- solutions/0007-reverse-integer/solution.cpp
+++ solutions/0007-reverse-integer/solution.cpp
@@ -6,8 +6,7 @@ public:
         while(x != 0){
             int rem = x%10;
 
-            if (s > INT_MAX / 10 || (s == INT_MAX / 10 && rem > 7)) return 0;
-            if (s < INT_MIN / 10 || (s == INT_MIN / 10 && rem < -8)) return 0;
+            if (s > INT_MAX / 10 || s < INT_MIN / 10) return 0;
 
             s = 10*s + rem;
             x/=10;
