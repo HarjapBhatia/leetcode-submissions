333e9e459781a4b2c3443eb29d576b97eac934a0
Harjap <harjapbhatia3@gmail.com>
2025-10-06 12:24:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.8 MB (41.09%)

---
+++ solutions/0001-two-sum/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> umap;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(umap.find(target-nums[i])!=umap.end()) return {i, umap[target-nums[i]]};
+            umap[nums[i]]=i;
+        }
+        return {};
+    }
+};
