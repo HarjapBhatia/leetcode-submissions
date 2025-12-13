fb1ad14d304be0d2fa21aac9e0b00ea44cdb22ad
Harjap <harjapbhatia3@gmail.com>
2025-12-13 19:36:19 +0000

    Sync LeetCode submission Runtime - 4 ms (77.25%), Memory - 128.4 MB (34.05%)

---
+++ solutions/2271-rearrange-array-elements-by-sign/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>vec(nums.size(), 0);
+        int p=0,n=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] > 0) {vec[p] = nums[i]; p+=2;}
+            else {vec[n] = nums[i]; n+=2;}
+        }
+        return vec;
+    }
+};
