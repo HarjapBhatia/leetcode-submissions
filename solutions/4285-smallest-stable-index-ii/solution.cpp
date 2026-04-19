1984e53281d37dede1ac641ce800906654eb10dc
Harjap <harjapbhatia3@gmail.com>
2026-04-19 03:45:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 202.7 MB (77.04%)

---
+++ solutions/4285-smallest-stable-index-ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> sm(n);
+        sm[n-1] = nums[n-1];
+        // int mi = sm[n-1];
+        for(int i=n-2;i>=0;i--){
+            sm[i] = min(nums[i], sm[i+1]);
+        }
+        int mx = 0;
+        for(int i=0;i<n;i++){
+            mx = max(mx, nums[i]);
+            if(mx - sm[i] <= k) return i;
+        }
+        return -1;
+    }
+};
