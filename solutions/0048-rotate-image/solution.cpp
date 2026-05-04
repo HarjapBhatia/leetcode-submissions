a9dbb49b820053fa4e552ae1e52254d0c39db265
Harjap <harjapbhatia3@gmail.com>
2026-05-04 03:02:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (94.04%)

--- solutions/0048-rotate-image/solution.cpp
+++ solutions/0048-rotate-image/solution.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j], matrix[j][i]);
-                if(i == n/2 && j == n/2) return;
             }
         }
         for(auto &i : matrix) reverse(i.begin(), i.end());
