e1b43d3c62a6e0a01a0546b80724ea492bcba423
Harjap <harjapbhatia3@gmail.com>
2026-05-14 03:48:21 +0000

    Sync LeetCode submission Runtime - 10 ms (13.51%), Memory - 24.8 MB (14.17%)

--- solutions/0056-merge-intervals/solution.cpp
+++ solutions/0056-merge-intervals/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& it) {
+    vector<vector<int>> merge(vector<vector<int>>& iv) {
         vector<vector<int>> res;
-        sort(it.begin(), it.end());
-        auto prev = it[0]; 
-        for(int i=1; i<it.size();i++){
-            auto cur = it[i];
-            if(prev[1] >= cur[0]){
-                // prev = {prev[0], max(prev[1], cur[1])};
-                prev[1] = max(prev[1], cur[1]);
-            }
+        sort(iv.begin(), iv.end());
+        auto prv = iv[0];
+        for(int i=1;i<iv.size();i++){
+            auto cur = iv[i];
+            if(cur[0] <= prv[1]) 
+                prv = {prv[0], max(prv[1], cur[1])};
             else{
-                res.push_back(prev);
-                prev = cur;
+                res.push_back(prv);
+                prv = cur;
             }
         }
-        res.push_back(prev);
+        res.push_back(prv);
         return res;
     }
 };
