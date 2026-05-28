28a8e920bfdc5349b4edb09dcb4128bca23953d8
Harjap <harjapbhatia3@gmail.com>
2026-05-28 03:59:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.4 MB (47.00%)

--- solutions/0792-binary-search/solution.cpp
+++ solutions/0792-binary-search/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, h = nums.size() - 1;
-        while(l <= h){
-            int m = l + (h-l)/2;
+        int l=0, r=nums.size()-1;
+        while(l<=r){
+            int m = (l+r)/2;
             if(nums[m] == target) return m;
-            else if(nums[m] < target) l = m + 1;
-            else h = m - 1;
+            else if(nums[m] < target) l = m+1;
+            else r=m-1;
         }
         return -1;
     }
