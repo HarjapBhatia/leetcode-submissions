d8820f913d47b5eb862854014ff156dac5934aea
Harjap <harjapbhatia3@gmail.com>
2026-06-04 02:18:31 +0000

    Sync LeetCode submission Runtime - 49 ms (49.24%), Memory - 10.2 MB (40.28%)

--- solutions/4057-total-waviness-of-numbers-in-range-i/solution.cpp
+++ solutions/4057-total-waviness-of-numbers-in-range-i/solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int count(int n){
-        string s = to_string(n);
-        int sz = s.size();
-        int cnt = 0;
-        for(int i=1;i<sz-1;i++){
-            if((s[i] > s[i+1] && s[i] > s[i-1]) || (s[i] < s[i+1] && s[i] < s[i-1])) cnt++;    
+    int kitne(int x){
+        int cnt=0;
+        string s = to_string(x);
+        for(int i=1;i<s.size()-1;i++){
+            if((s[i]>s[i-1] && s[i]>s[i+1]) || 
+            ((s[i]<s[i-1]) && s[i]<s[i+1])) cnt++;
         }
         return cnt;
     }
     int totalWaviness(int num1, int num2) {
-        int res = 0;
+        int ans=0;
         for(int i=num1;i<=num2;i++){
-            res += count(i);
+            ans += kitne(i);
         }
-        return res;
+        return ans;
     }
 };
