3be763d555c1d90e75cdff11bdd59757c2b2c261
Harjap <harjapbhatia3@gmail.com>
2026-06-10 05:08:39 +0000

    Sync LeetCode submission Runtime - 2 ms (58.52%), Memory - 14 MB (94.12%)

--- solutions/0039-combination-sum/solution.cpp
+++ solutions/0039-combination-sum/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void fn(vector<int>&cd, vector<vector<int>>&res, vector<int>&vec, int t, int k, int i){
-        if(t == k) {res.push_back(vec); return;}
-        if(i >= cd.size() || t > k) return;
-        vec.push_back(cd[i]);
-        fn(cd, res, vec, t+cd[i], k, i); // for recusively adding the number and checking
-        vec.pop_back(); // in case of k > t, it will pop from the vec
-        fn(cd,res,vec,t,k,i+1);     // increase the index to add a new number and backtrack
+    void f(vector<int> &v, vector<int>&t, vector<vector<int>> &ans, int sm, int i, int k){
+        if(sm == k) {ans.push_back(t); return;}
+        if(i>=v.size() || sm > k) return;
+        t.push_back(v[i]);
+        f(v, t, ans, sm+v[i], i, k);
+        t.pop_back();
+        f(v, t, ans, sm, i+1, k);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> vec;
-        fn(candidates, res, vec, 0, target, 0);
-        return res;
+        vector<vector<int>> ans;
+        vector<int> t;
+        f(candidates, t, ans, 0, 0, target);
+        return ans;
     }
 };
