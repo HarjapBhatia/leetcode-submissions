1037d3cecad21fa920ffb0ee3bef2d51fffd7a81
Harjap <harjapbhatia3@gmail.com>
2026-06-20 12:23:27 +0000

    Sync LeetCode submission Runtime - 64 ms (56.24%), Memory - 44 MB (55.20%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -11,10 +11,11 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return nullptr;
-        if(root == p || root==q) return root;
-        TreeNode* lf = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rg = lowestCommonAncestor(root->right, p, q);
-        if(lf && rg) return root;
-        return lf ? lf : rg;
+        if(root==p || root==q) return root;
+        //postorder traversal
+        TreeNode* lf = lowestCommonAncestor(root->left, p,q);
+        TreeNode* rg = lowestCommonAncestor(root->right, p,q);
+        if(lf && rg) return root; // if both are there then return the ancestor
+        return lf ? lf : rg; // if there's only 1 (either p or q)
     }
 };
