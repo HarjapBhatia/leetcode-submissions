7c9d4c6f79112048fff9159d3698aa0c72198156
Harjap <harjapbhatia3@gmail.com>
2026-06-21 05:54:32 +0000

    Sync LeetCode submission Runtime - 1 ms (93.65%), Memory - 84.4 MB (24.21%)

--- solutions/1961-maximum-ice-cream-bars/solution.cpp
+++ solutions/1961-maximum-ice-cream-bars/solution.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
-        int i=0, sm=0;
-        for(i=0;i<costs.size();i++){
-            if(coins >= (sm+costs[i])) sm += costs[i];
-            else break; 
+    int maxIceCream(vector<int>& c, int coins) {
+        int mx = *max_element(c.begin(), c.end());
+        vector<int> cnt(mx+1,0);
+        for(int i : c) cnt[i]++;
+        int ans =0;
+
+        for(int i=1;i<=mx;i++){
+            int can = min(coins/i, cnt[i]);
+            ans += can;
+            coins -= can*i;
         }
-        return i;
+        return ans;
     }
 };
