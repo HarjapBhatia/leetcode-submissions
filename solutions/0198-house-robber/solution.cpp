27703dd1c4f3ef7a27208d1d9d1839aea04f4da0
Harjap <harjapbhatia3@gmail.com>
2026-06-26 05:30:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (47.29%)

--- solutions/0198-house-robber/solution.cpp
+++ solutions/0198-house-robber/solution.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+
+    int f(vector<int> &v, int i, vector<int>&dp){
+        if(i>=v.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = max(v[i]+f(v,i+2, dp), f(v,i+1, dp));
+    }
+
+
     int rob(vector<int>& nums) {
         // method : 1 
         // int n=nums.size();
@@ -11,13 +19,19 @@ public:
         // }
         // return dp[n-1];
 
-        //method : 2
-        int p1=0, p2=0;
-        for(int x: nums){
-            int cur = max(p1, p2+x);
-            p2 = p1;
-            p1 = cur;
-        }
-        return p1;
+        //method : 2 (optimal)
+        // int p1=0, p2=0;
+        // for(int x: nums){
+        //     int cur = max(p1, p2+x);
+        //     p2 = p1;
+        //     p1 = cur;
+        // }
+        // return p1;
+
+        //method 0: rec + dp
+        vector<int> dp(nums.size()+1,-1);
+        if(nums.size() <= 2) 
+            return *max_element(nums.begin(), nums.end());
+        return f(nums, 0, dp);
     }
 };
