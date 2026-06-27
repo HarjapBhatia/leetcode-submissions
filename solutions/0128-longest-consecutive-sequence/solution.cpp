a252d951041df9ce084e2f048e9f90aa82ef6567
Harjap <harjapbhatia3@gmail.com>
2026-06-27 06:29:16 +0000

    Sync LeetCode submission Runtime - 82 ms (51.47%), Memory - 88.9 MB (49.76%)

--- solutions/0128-longest-consecutive-sequence/solution.cpp
+++ solutions/0128-longest-consecutive-sequence/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>uset(nums.begin(), nums.end());
-        int maxlen = 0;
-        for (int num : uset){
-            int currElement = num;
-            if(uset.find(num-1)==uset.end()){
-                int currLength = 1;
-                while(uset.find(currElement+1)!=uset.end()){
-                    currLength++;
-                    currElement++;      
-                }
-                maxlen = max(maxlen,currLength);
+        unordered_set<int> us(nums.begin(), nums.end());
+        int ans=0;
+        for(int i : us){
+            int x = i;
+            if(x == INT_MIN || us.find(x-1)==us.end()){
+                int cur = 1;
+                while(x != INT_MAX && us.find(x+1)!=us.end()) {cur++; x++;}
+                ans = max(ans, cur);
             }
         }
-        return maxlen;
+        return ans;
     }
 };
