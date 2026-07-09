360a8fb0b34652835ffadba0bc2e31492c12c589
Harjap <harjapbhatia3@gmail.com>
2026-07-09 08:27:55 +0000

    Sync LeetCode submission Runtime - 3 ms (27.70%), Memory - 22.1 MB (42.66%)

--- solutions/0735-asteroid-collision/solution.cpp
+++ solutions/0735-asteroid-collision/solution.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& a) {
+    vector<int> asteroidCollision(vector<int>& ast) {
         stack<int> st;
-        int n = a.size();
-        for (int x : a) {
-            bool f = false;
-            while(!st.empty() && x < 0 && st.top() > 0){
-                if(abs(x) == abs(st.top())){
-                    st.pop(); f=true; break;
-                }else if(abs(st.top()) < abs(x)) st.pop();
-                else {f=true; break;}
+        for(int x : ast){
+            bool f=false;
+            while(!st.empty() && x<0 && st.top()>0){
+                if(abs(st.top())==abs(x)){st.pop(); f=true; break;}
+                else if(abs(x) > abs(st.top())) st.pop();
+                else{f=true; break;}
             }
             if(!f) st.push(x);
         }
-        vector<int> res;
-        while(!st.empty()){
-            res.push_back(st.top());
-            st.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        vector<int> ans;
+        while(!st.empty()) {ans.push_back(st.top()); st.pop();}
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
