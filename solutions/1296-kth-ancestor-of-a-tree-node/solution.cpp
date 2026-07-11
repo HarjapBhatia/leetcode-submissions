1de7ec4d1481c83c13184f257ddb1ad9d39b4f49
Harjap <harjapbhatia3@gmail.com>
2026-07-11 19:29:05 +0000

    Sync LeetCode submission Runtime - 70 ms (22.51%), Memory - 122.4 MB (92.94%)

---
+++ solutions/1296-kth-ancestor-of-a-tree-node/solution.cpp
@@ -0,0 +1,32 @@
+class TreeAncestor {
+public:
+    vector<vector<int>>up;
+    int LOG=0;
+    TreeAncestor(int n, vector<int>& parent) {
+        while((1<<LOG) <= n) LOG++;
+        up = vector<vector<int>>(n, vector<int>(LOG, -1));    
+        for(int i=0;i<n;i++)
+            up[i][0] = parent[i];
+        for(int j=1;j<LOG;j++){
+            for(int i=0;i<n;i++){
+                if(up[i][j-1] != -1) up[i][j] = up[up[i][j-1]][j-1];
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        for(int i=LOG-1;i>=0;i--){
+            if(k & (1<<i)){
+                node = up[node][i];
+                if(node == -1) return -1; 
+            }
+        }
+        return node;
+    }
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
