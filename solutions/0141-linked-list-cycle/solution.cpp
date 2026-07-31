213617aa5d9dec08af0ea36b5963dd61ad85efa8
Harjap <harjapbhatia3@gmail.com>
2026-07-31 05:30:38 +0000

    Sync LeetCode submission Runtime - 4 ms (97.26%), Memory - 11.9 MB (54.55%)

--- solutions/0141-linked-list-cycle/solution.cpp
+++ solutions/0141-linked-list-cycle/solution.cpp
@@ -9,13 +9,10 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(!head || head -> next == NULL) return false;
-        ListNode *s = head;
-        ListNode *f = head;
-        while(f && f->next){
-            s = s->next;
-            f = f->next->next;
-            if (s == f) return true;
+        ListNode* f=head, *s=head;
+        while(s && f && f->next){
+            f=f->next->next; s=s->next;
+            if(s==f) return true;
         }
         return false;
     }
