d575f630125d37b17885d9d529c654ed826a68d8
Harjap <harjapbhatia3@gmail.com>
2026-07-31 06:02:16 +0000

    Sync LeetCode submission Runtime - 5 ms (33.28%), Memory - 118.1 MB (65.07%)

--- solutions/0234-palindrome-linked-list/solution.cpp
+++ solutions/0234-palindrome-linked-list/solution.cpp
@@ -10,22 +10,42 @@
  */
 class Solution {
 public:
-    bool check(vector<int> vec){
-        if(vec.size() == 1) return true;
-        int i=0, j=vec.size()-1;
-        while(i<=j){
-            if(vec[i]!=vec[j]) return false;
-            i++;
-            j--;
+    // bool check(vector<int> vec){
+    //     if(vec.size() == 1) return true;
+    //     int i=0, j=vec.size()-1;
+    //     while(i<=j){
+    //         if(vec[i]!=vec[j]) return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    bool isPalindrome(ListNode* head) {
+        ListNode* f=head, *s=head;
+        while(f && f->next){
+            f=f->next->next; s=s->next;
+        }
+        ListNode* prev=s;
+        s=s->next;
+        prev->next = nullptr;
+        while(s){
+            ListNode* temp = s->next;
+            s->next = prev;
+            prev = s;
+            s = temp;
+        }
+        f=head; s=prev;
+        while(s){
+            if(f->val!=s->val) return false;
+            f=f->next; s=s->next;
         }
         return true;
-    }
-    bool isPalindrome(ListNode* head) {
-        vector<int> vec;
-        ListNode*temp = head;
-        while(temp != nullptr){
-          vec.push_back(temp->val), temp = temp->next;  
-        }
-        return check(vec);    
+
+        // vector<int> vec;
+        // ListNode*temp = head;
+        // while(temp != nullptr){
+        //   vec.push_back(temp->val), temp = temp->next;  
+        // }
+        // return check(vec);    
     }
 };
